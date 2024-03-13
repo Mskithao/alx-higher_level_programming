9eac3b62454e34698c40432182a966057624f3d8
Mskithao <patricaikithao@gmail.com>
2024-03-13 09:09:10 -0700

    fix 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    return 97 <= ord(c) <= 122
+for digit1 in range(0, 9):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 < 8:
+            print("{:02d}, ".format(digit1 * 10 + digit2), end="")
+        else:
+            print("{:02d}".format(digit1 * 10 + digit2))

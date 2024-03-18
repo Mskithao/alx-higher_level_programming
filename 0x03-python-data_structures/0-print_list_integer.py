8c5018c9cbbaad652d857c3b1e846509cd10f8c4
Mskithao <patricaikithao@gmail.com>
2024-03-18 11:31:17 -0700

    my list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#above is a shebang, indicating the path to the python interpretor
+
+#prototype: function defination - 'print_list_interger' taking 1 argument'my_list' initialized with an empty list by default
+def print_list_integer(my_list=[]):
+
+    #iterate through each element in 'my_list'.
+    for num in my_list:
+        #print each interger element from 'my_list' using string formating to ensure it is printed as an integer
+        print("{:d}".format(num))
+
+        #the lines below test the code
+        my_list = [1, 2, 3, 4, 5]
+        print_list_integer(my_list)

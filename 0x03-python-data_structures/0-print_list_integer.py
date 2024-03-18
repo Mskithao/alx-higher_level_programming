553dbecc64d943da23a0eedf6d9f54eeae703251
Mskithao <patricaikithao@gmail.com>
2024-03-18 11:45:22 -0700

    my list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-#above is a shebang, indicating the path to the python interpretor
-
-#prototype: function defination - 'print_list_interger' taking 1 argument'my_list' initialized with an empty list by default
 def print_list_integer(my_list=[]):
-
-    #iterate through each element in 'my_list' using range function
     for i in range(len(my_list)):
-        #print each interger element from 'my_list' using string formating to ensure it is printed as an integer
-        print("{:d}".format(my-list[i]))
-
+        print("{:d}".format(my_list[i]))

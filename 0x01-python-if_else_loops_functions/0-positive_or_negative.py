d7ede0c8ad8803fdbedc68bc1c16f554b2d1afe4
Mskithao <patricaikithao@gmail.com>
2024-04-12 03:54:56 -0700

    see why positive is bteer now?

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 
-# Assign a random signed integer to the variable number
-number = random.randint(-100, 100)
+number = random.randint(-10, 10)
 
-# Check if the number is positive, negative, or zero and print the appropriate message
+# Determine the status of the number and print the appropriate message
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
